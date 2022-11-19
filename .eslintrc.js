aa0b239bc6c29e20b7c47894a01b14ea5f41859e
Robin Bron <robin@finwo.nl>
2022-11-19 16:28:22 +0100

    Added eslint as linter

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'indent': [ 'error', 2 ],
+    'linebreak-style': [ 'error', 'unix' ],
+    'quotes': [ 'error', 'single' ],
+    'semi': [ 'error', 'always' ],
+    'comma-dangle': [ 'error', 'always-multiline' ],
+    '@typescript-eslint/no-explicit-any': 0,
+  },
+};
